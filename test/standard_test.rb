35d87692803a10912f67f3c33f39509514864288
Justin Searls <searls@gmail.com>
2018-11-07 13:18:53 -0500

    delete failing test

--- test/standard_test.rb
+++ test/standard_test.rb
@@ -4,8 +4,4 @@ class StandardTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Standard::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
