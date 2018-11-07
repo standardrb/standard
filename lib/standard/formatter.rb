21ad7100a8ed81b624c6f1e9f2977e4dfcbb5989
Justin Searls <searls@gmail.com>
2018-11-07 09:29:17 -0500

    print the header just once

--- lib/standard/formatter.rb
+++ lib/standard/formatter.rb
@@ -3,16 +3,22 @@ require "rubocop"
 module Standard
   class Formatter < RuboCop::Formatter::BaseFormatter
     def file_finished(file, offenses)
-      if offenses.size > 0
-        output.print <<~NOTE
-          standard: Use Ruby Standard Style (https://github.com/testdouble/standard)
-          standard: Run `standard --fix` to automatically fix some problems.
-        NOTE
-      end
+      print_header_once if offenses.size > 0
 
       offenses.each do |o|
         output.printf("  %s:%d:%d: %s\n", file, o.line, o.real_column, o.message.tr("\n", ' '))
       end
+    end
+
+    private
+
+    def print_header_once
+      return if @header_printed_already
+      output.print <<~HEADER
+          standard: Use Ruby Standard Style (https://github.com/testdouble/standard)
+          standard: Run `standard --fix` to automatically fix some problems.
+      HEADER
+      @header_printed_already = true
     end
   end
 end
