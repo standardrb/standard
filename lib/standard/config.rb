2f8a214f95ef51ee6a5f9b3dbad1c19bd33a153c
Justin Searls <searls@gmail.com>
2018-11-07 09:51:08 -0500

    Support setting ruby_version
    Also, instead of looking at .ruby-version (which weirdly is not working
    regardless), just default to the currently running version of Ruby.
    
    Also, why the heck does RuboCop demand you provide the ruby version as
    a float? Bizarro.

--- lib/standard/config.rb
+++ lib/standard/config.rb
@@ -35,7 +35,8 @@ module Standard
         :fix => !!user_config["fix"],
         :format => user_config["format"],
         :ignore => expand_ignore_config(user_config["ignore"]),
-        :parallel => !!user_config["parallel"]
+        :parallel => !!user_config["parallel"],
+        :ruby_version => ruby_version(user_config["ruby_version"] || RUBY_VERSION)
       }
     end
 
@@ -51,6 +52,8 @@ module Standard
     def mutate_config_store!(config_store)
       config_store.options_config = Pathname.new(__dir__).join("../../config/base.yml")
       options_config = config_store.instance_variable_get("@options_config")
+
+      options_config["AllCops"]["TargetRubyVersion"] = @standard_config[:ruby_version]
 
       @standard_config[:ignore].each do |(path, cops)|
         cops.each do |cop|
@@ -69,6 +72,11 @@ module Standard
           rule.entries.first
         end
       }
+    end
+
+    def ruby_version(version)
+      major, minor = Gem::Version.new(version).segments
+      "#{major}.#{minor}".to_f # lol
     end
 
     def arrayify(object)
