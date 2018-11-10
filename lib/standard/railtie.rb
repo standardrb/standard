8877d3f043b68685b2625fafd7404c3b54c5ae91
Justin Searls <searls@gmail.com>
2018-11-10 14:36:46 -0500

    Add rake tasks and a railtie

---
+++ lib/standard/railtie.rb
@@ -0,0 +1,11 @@
+require "pathname"
+
+module Standard
+  class Railtie < Rails::Railtie
+    railtie_name :standard
+
+    rake_tasks do
+      load Pathname.new(__dir__).join("rake.rb")
+    end
+  end
+end
