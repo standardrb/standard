f793685362529d1e41d6ccf84aadbbd3b61c8dd7
Emiliano Leite <emiliano.leite@codus.com.br>
2019-02-11 12:54:09 -0200

    Only suggest autocorrect for safe cops
    Since Standard always sets the `safe_auto_correct` flag when
    autocorrecting offenses, it makes sense to check if the offense
     is safely autocorrectable before suggesting the command.

--- lib/standard/helpers/autocorrectable.rb
+++ lib/standard/helpers/autocorrectable.rb
@@ -2,7 +2,8 @@ module Standard
   module Helpers
     module Autocorrectable
       def autocorrectable_offense?(offense)
-        cop_instance(offense.cop_name).support_autocorrect?
+        cop = cop_instance(offense.cop_name)
+        cop.support_autocorrect? && safely_autocorrectable?(cop)
       end
 
       def cop_instance(cop_name)
@@ -12,6 +13,11 @@ module Standard
       def constantized_cop(cop_name)
         namespaced_cop_class = cop_name.to_s.gsub("/", "::")
         RuboCop::Cop.const_get(namespaced_cop_class)
+      end
+
+      def safely_autocorrectable?(cop)
+        return true unless cop.cop_config
+        cop.cop_config.fetch("SafeAutoCorrect", true)
       end
     end
   end
