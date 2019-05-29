d5e91f00335cf16f2dca56a64841f18b3a35a0b5
Justin Searls <searls@gmail.com>
2019-05-29 18:24:25 -0400

    Write our first test directly of a cop.
    This was way too much effort.

---
+++ test/cop_invoker.rb
@@ -0,0 +1,185 @@
+# Most of this was adapted from Rubocop's
+#
+# lib/rubocop/rspec/expect_offense.rb
+module CopInvoker
+  RUBY_VERSION = 2.2
+
+  # Usage:
+  #
+  #   assert_offense(<<~RUBY)
+  #     a do
+  #       b
+  #     end.c
+  #     ^^^^^ Avoid chaining a method call on a do...end block.
+  #   RUBY
+  #
+  def assert_offense(cop, source)
+    RuboCop::Formatter::DisabledConfigFormatter.config_to_allow_offenses = {}
+    RuboCop::Formatter::DisabledConfigFormatter.detected_styles = {}
+    cop.instance_variable_get(:@options)[:auto_correct] = true
+    expected = AnnotatedSource.parse(source)
+
+    @last_source = RuboCop::ProcessedSource.new(
+      expected.plain_source, RUBY_VERSION, nil
+    )
+
+    raise "Error parsing example code" unless @last_source.valid_syntax?
+
+    _investigate(cop, @last_source)
+
+    actual_annotations = expected.with_offense_annotations(cop.offenses)
+    assert_equal expected.to_s, actual_annotations.to_s
+  end
+
+  # Auto-correction can be tested using `assert_correction` after
+  # `assert_offense`:
+  #
+  #   assert_offense(<<~RUBY)
+  #     x % 2 == 0
+  #     ^^^^^^^^^^ Replace with `Integer#even?`.
+  #   RUBY
+  #
+  #   assert_correction(<<~RUBY)
+  #     x.even?
+  #   RUBY
+  #
+  def assert_correction(cop, expected)
+    raise "`assert_correction` must follow `assert_offense`" unless @last_source
+
+    actual = RuboCop::Cop::Corrector.new(@last_source.buffer, cop.corrections).rewrite
+
+    assert_equal expected, actual
+  end
+
+  def assert_no_correction(cop)
+    raise "`assert_no_correction` must follow `assert_offense`" unless @last_source
+
+    return if cop.corrections.empty?
+
+    actual = RuboCop::Cop::Corrector.new(@last_source.buffer, cop.corrections).rewrite
+
+    assert_equal @last_source.buffer.source, actual
+  end
+
+  def assert_no_offense(cop, source)
+    RuboCop::Formatter::DisabledConfigFormatter.config_to_allow_offenses = {}
+    RuboCop::Formatter::DisabledConfigFormatter.detected_styles = {}
+    processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION, nil)
+
+    raise "Error parsing example code" unless processed_source.valid_syntax?
+
+    _investigate(cop, processed_source)
+
+    assert_equal [], cop.offenses
+  end
+
+  # Parsed representation of code annotated with the `^^^ Message` style
+  class AnnotatedSource
+    ANNOTATION_PATTERN = /\A\s*\^+ /.freeze
+
+    # @param annotated_source [String] string passed to the matchers
+    #
+    # Separates annotation lines from source lines. Tracks the real
+    # source line number that each annotation corresponds to.
+    #
+    # @return [AnnotatedSource]
+    def self.parse(annotated_source)
+      source      = []
+      annotations = []
+
+      annotated_source.each_line do |source_line|
+        if source_line =~ ANNOTATION_PATTERN
+          annotations << [source.size, source_line]
+        else
+          source << source_line
+        end
+      end
+
+      new(source, annotations)
+    end
+
+    # @param lines [Array<String>]
+    # @param annotations [Array<(Integer, String)>]
+    #   each entry is the annotated line number and the annotation text
+    #
+    # @note annotations are sorted so that reconstructing the annotation
+    #   text via {#to_s} is deterministic
+    def initialize(lines, annotations)
+      @lines       = lines.freeze
+      @annotations = annotations.sort.freeze
+    end
+
+    # Construct annotated source string (like what we parse)
+    #
+    # Reconstruct a deterministic annotated source string. This is
+    # useful for eliminating semantically irrelevant annotation
+    # ordering differences.
+    #
+    # @example standardization
+    #
+    #     source1 = AnnotatedSource.parse(<<-RUBY)
+    #     line1
+    #     ^ Annotation 1
+    #      ^^ Annotation 2
+    #     RUBY
+    #
+    #     source2 = AnnotatedSource.parse(<<-RUBY)
+    #     line1
+    #      ^^ Annotation 2
+    #     ^ Annotation 1
+    #     RUBY
+    #
+    #     source1.to_s == source2.to_s # => true
+    #
+    # @return [String]
+    def to_s
+      reconstructed = lines.dup
+
+      annotations.reverse_each do |line_number, annotation|
+        reconstructed.insert(line_number, annotation)
+      end
+
+      reconstructed.join
+    end
+
+    # Return the plain source code without annotations
+    #
+    # @return [String]
+    def plain_source
+      lines.join
+    end
+
+    # Annotate the source code with the RuboCop offenses provided
+    #
+    # @param offenses [Array<RuboCop::Cop::Offense>]
+    #
+    # @return [self]
+    def with_offense_annotations(offenses)
+      offense_annotations =
+        offenses.map { |offense|
+          indent = " " * offense.column
+          carets = "^" * offense.column_length
+
+          [offense.line, "#{indent}#{carets} #{offense.message}\n"]
+        }
+
+      self.class.new(lines, offense_annotations)
+    end
+
+    private
+
+    attr_reader :lines, :annotations
+  end
+
+  def _investigate(cop, last_source)
+    forces = RuboCop::Cop::Force.all.each_with_object([]) { |klass, instances|
+      next unless cop.join_force?(klass)
+
+      instances << klass.new([cop])
+    }
+
+    commissioner = RuboCop::Cop::Commissioner.new([cop], forces, raise_error: true)
+    commissioner.investigate(last_source)
+    commissioner
+  end
+end
---
+++ test/standard/cop/semantic_blocks_test.rb
@@ -0,0 +1,60 @@
+require "test_helper"
+require "cop_invoker"
+
+class RuboCop::Cop::Standard::SemanticBlocksTest < UnitTest
+  include CopInvoker
+
+  def setup
+    standard_config = YAML.load_file("config/base.yml")["Standard/SemanticBlocks"]
+    config = RuboCop::Config.new("Standard/SemanticBlocks" => standard_config.merge(
+      "Enabled" => true
+    ))
+    @cop = RuboCop::Cop::Standard::SemanticBlocks.new(config)
+  end
+
+  def test_do_end_with_assignment_fails
+    assert_offense @cop, <<-RUBY
+      lulz = [:a].map do
+                      ^^ Prefer `{...}` over `do...end` for functional blocks.
+        :lol
+      end
+    RUBY
+
+    assert_correction @cop, <<-RUBY
+      lulz = [:a].map {
+        :lol
+      }
+    RUBY
+  end
+
+  def test_braces_with_assignment_passes
+    assert_no_offense @cop, <<-RUBY
+      lulz = [:a].map {
+        :lol
+      }
+    RUBY
+  end
+
+  def test_braces_with_side_effect_fails
+    assert_offense @cop, <<-RUBY
+      42.tap {
+             ^ Prefer `do...end` over `{...}` for procedural blocks.
+        puts "cool"
+      }
+    RUBY
+
+    assert_correction @cop, <<-RUBY
+      42.tap do
+        puts "cool"
+      end
+    RUBY
+  end
+
+  def test_do_end_with_side_effect_passes
+    assert_no_offense @cop, <<-RUBY
+      42.tap do
+        puts "cool"
+      end
+    RUBY
+  end
+end
