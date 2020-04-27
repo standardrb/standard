cf67006749823252d0bfdb03fb39bf9981c08807
Justin Searls <searls@gmail.com>
2020-04-27 14:19:33 +0900

    Be extra careful about detecting Rails
    Turns out if a gem or some code has a constant named Rails defined this
    will (naturally) blow up, which isn't ideal

--- lib/standard.rb
+++ lib/standard.rb
@@ -4,7 +4,7 @@ require "standard/rubocop/ext"
 
 require "standard/version"
 require "standard/cli"
-require "standard/railtie" if defined?(Rails)
+require "standard/railtie" if defined?(Rails) && defined?(Rails::Railtie)
 
 require "standard/formatter"
 require "standard/cop/semantic_blocks"
