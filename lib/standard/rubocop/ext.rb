89114d69fc1b6c02908517970e691f4f13a3fe9e
Odin Dutton <odindutton@gmail.com>
2020-06-21 17:28:15 +1000

    Rename comment directive to "standardrb"

--- lib/standard/rubocop/ext.rb
+++ lib/standard/rubocop/ext.rb
@@ -5,4 +5,12 @@ module RuboCop
       "Wrap assignment in parentheses if intentional"
     end
   end
+
+  class CommentConfig
+    remove_const :COMMENT_DIRECTIVE_REGEXP
+    COMMENT_DIRECTIVE_REGEXP = Regexp.new(
+      ('# standardrb : ((?:disable|enable|todo))\b ' + COPS_PATTERN)
+        .gsub(" ", '\s*')
+    )
+  end
 end
