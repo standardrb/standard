e872688293fe4a3d583545df06cf1b9cc6ddd6b4
Nick Sieger <nick@nicksieger.com>
2021-03-05 12:18:23 -0600

    Use RuboCop::CLI::Command::ExecuteRunner
    Preserves RuboCop exit status while using RuboCop logic for printing
    summary, either on stdout or stderr with `--stderr` option.

--- lib/standard/runners/rubocop.rb
+++ lib/standard/runners/rubocop.rb
@@ -4,36 +4,15 @@ module Standard
   module Runners
     class Rubocop
       def call(config)
-        rubocop_runner = RuboCop::Runner.new(
-          config.rubocop_options,
-          config.rubocop_config_store
+        rubocop_runner = RuboCop::CLI::Command::ExecuteRunner.new(
+          RuboCop::CLI::Environment.new(
+            config.rubocop_options,
+            config.rubocop_config_store,
+            config.paths
+          )
         )
 
-        rubocop_runner.run(config.paths).tap do |success|
-          print_errors_and_warnings(success, rubocop_runner)
-          print_corrected_code_if_fixing_stdin(config.rubocop_options)
-        end
-      end
-
-      private
-
-      def print_errors_and_warnings(success, rubocop_runner)
-        return unless success
-
-        (rubocop_runner.warnings + rubocop_runner.errors).each do |message|
-          warn message
-        end
-      end
-
-      def print_corrected_code_if_fixing_stdin(rubocop_options)
-        return unless rubocop_options[:stdin] && rubocop_options[:auto_correct]
-
-        if rubocop_options[:stderr]
-          warn "=" * 20
-        else
-          puts "=" * 20
-        end
-        print rubocop_options[:stdin]
+        rubocop_runner.run
       end
     end
   end
