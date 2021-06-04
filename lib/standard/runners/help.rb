c1e8554e77914c70eaac95284578cf2be215f3b6
Jamie Alessio <jamie@stoic.net>
2021-06-04 09:15:17 -0700

    Add '--generate-todo' to CLI help output

--- lib/standard/runners/help.rb
+++ lib/standard/runners/help.rb
@@ -12,6 +12,7 @@ module Standard
             --fix             Automatically fix failures where possible
             --no-fix          Do not automatically fix failures
             --format <name>   Format output with any RuboCop formatter (e.g. "json")
+            --generate-todo   Create a .standard_todo.yml that lists all the files that contain errors
             -v, --version     Print the version of Standard
             -h, --help        Print this message
             FILE              Files to lint [default: ./]
