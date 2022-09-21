21e067433f8d8cb3199dac4ef45eb0a06aa78f41
Chris C <chris.cumming@saturdaymp.com>
2022-09-20 20:07:08 -0600

    Make the min target ruby version a constant.

--- lib/standard/creates_config_store/sets_target_ruby_version.rb
+++ lib/standard/creates_config_store/sets_target_ruby_version.rb
@@ -1,5 +1,14 @@
 class Standard::CreatesConfigStore
   class SetsTargetRubyVersion
+    # This is minimum version that Rubocop can parse, not the minimum
+    # version it can run on (e.g. TargetRubyVersion).  See the following
+    # for more details:
+    #
+    # https://docs.rubocop.org/rubocop/configuration.html#setting-the-target-ruby-version
+    #
+    # https://github.com/rubocop/rubocop/blob/master/lib/rubocop/target_ruby.rb
+    MIN_TARGET_RUBY_VERSION = "2.0"
+
     def call(options_config, standard_config)
       options_config["AllCops"]["TargetRubyVersion"] = normalize_version(
         min_target_ruby_version_supported(standard_config[:ruby_version])
@@ -11,14 +20,7 @@ class Standard::CreatesConfigStore
     def min_target_ruby_version_supported(desired_target_ruby_version)
       return desired_target_ruby_version unless Gem::Version.correct?(desired_target_ruby_version)
 
-      # This is minimum version that Rubocop can parse, not the minimum
-      # version it can run on (e.g. TargetRubyVersion).  See the following
-      # for more details:
-      #
-      # https://docs.rubocop.org/rubocop/configuration.html#setting-the-target-ruby-version
-      #
-      # https://github.com/rubocop/rubocop/blob/master/lib/rubocop/target_ruby.rb
-      min_target_ruby_version = Gem::Version.new("2.0")
+      min_target_ruby_version = Gem::Version.new(MIN_TARGET_RUBY_VERSION)
       if desired_target_ruby_version < min_target_ruby_version
         min_target_ruby_version
       else
