f1983d225a298a6369792ffa0721f55362450e47
André Costa <andreccosta@me.com>
2022-12-23 00:45:37 +0000

    Add test for isolated comment directive

---
+++ test/fixture/comment_directive_test/isolated.rb
@@ -0,0 +1,7 @@
+foo = 42 # standard:disable Lint/UselessAssignment
+
+# standard:disable Layout/IndentationWidth
+def foo
+    123
+end
+# standard:enable Layout/IndentationWidth
--- test/standard/comment_directive_test.rb
+++ test/standard/comment_directive_test.rb
@@ -15,4 +15,14 @@ class Standard::CommentDirectiveTest < UnitTest
         test/fixture/comment_directive_test/disabled.rb:11:1: Layout/IndentationWidth: Use 2 (not 4) spaces for indentation.
     OUTPUT
   end
+
+  def test_isolated_directive
+    fake_out, fake_err, exit_code = do_with_fake_io {
+      Standard::Cli.new(["test/fixture/comment_directive_test/isolated.rb"]).run
+    }
+
+    assert_equal 0, exit_code
+    assert_empty fake_err.string
+    assert_empty fake_out.string
+  end
 end
