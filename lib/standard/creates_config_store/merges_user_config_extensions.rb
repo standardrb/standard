bf990d82654b45b10ac22a2a4f58be4dcdad0f2a
Justin Searls <searls@gmail.com>
2023-01-14 13:28:00 -0500

    fix 2.7

--- lib/standard/creates_config_store/merges_user_config_extensions.rb
+++ lib/standard/creates_config_store/merges_user_config_extensions.rb
@@ -26,21 +26,25 @@ class Standard::CreatesConfigStore
         standard_config[:extend_config].each do |path|
           extension = RuboCop::ConfigLoader.load_file(Standard::FileFinder.new.call(path, Dir.pwd)).to_h
           config["AllCops"].merge!(extension["AllCops"]) if extension.key?("AllCops")
-          config.merge!(extension.except("AllCops"))
+          config.merge!(except(extension, ["AllCops"]))
         end
       end
     end
 
     def merge_standard_and_user_all_cops!(options_config, extended_config)
       options_config["AllCops"].merge!(
-        extended_config["AllCops"].except(*DISALLOWED_ALLCOPS_KEYS)
+        except(extended_config["AllCops"], DISALLOWED_ALLCOPS_KEYS)
       )
     end
 
     def merge_extended_rules_into_standard!(options_config, extended_config)
-      extended_config.except(*options_config.keys).each do |key, value|
+      except(extended_config, options_config.keys).each do |key, value|
         options_config[key] = value
       end
+    end
+
+    def except(hash, keys)
+      hash.reject { |key, _| keys.include?(key) }.to_h
     end
   end
 end
