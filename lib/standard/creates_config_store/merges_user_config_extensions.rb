5444b9d7cf8e8eff87f229236a77e23a7e86adb1
Justin Searls <searls@gmail.com>
2023-01-18 17:56:02 -0500

    Refactor code  to shorten the method a bit and use extraction over comments where we can

--- lib/standard/creates_config_store/merges_user_config_extensions.rb
+++ lib/standard/creates_config_store/merges_user_config_extensions.rb
@@ -6,13 +6,17 @@ class Standard::CreatesConfigStore
       "Include",
       "Exclude",
       "StyleGuideCopsOnly",
-      "TargetRubyVersion"
+      "TargetRubyVersion",
+
+      # The AllCops[Enabled] key is an unused artifact of #merge_with_default.
+      # See: https://github.com/rubocop/rubocop/blob/master/lib/rubocop/config_loader_resolver.rb#L81-L85
+      "Enabled"
     ].freeze
 
     def call(options_config, standard_config)
       return unless standard_config[:extend_config]&.any?
 
-      extended_config = load_and_merge_extended_rubocop_configs(options_config, standard_config)
+      extended_config = load_and_merge_extended_rubocop_configs(options_config, standard_config).to_h
       merge_standard_and_user_all_cops!(options_config, extended_config)
       merge_extended_rules_into_standard!(options_config, extended_config)
     end
@@ -20,35 +24,15 @@ class Standard::CreatesConfigStore
     private
 
     def load_and_merge_extended_rubocop_configs(options_config, standard_config)
-      orig_default_config = RuboCop::ConfigLoader.instance_variable_get(:@default_configuration)
+      fake_out_rubocop_default_configuration(options_config) do |fake_config|
+        standard_config[:extend_config].reduce(fake_config) do |config, path|
+          RuboCop::ConfigLoader.instance_variable_set(:@default_configuration, config)
 
-      # Blank configuration object to merge extensions into, with all known
-      # AllCops keys set to avoid warnings about unknown properties
-      config = RuboCop::Config.new(options_config.to_h.slice("AllCops"), "")
-      RuboCop::ConfigLoader.instance_variable_set(:@default_configuration, config)
-
-      standard_config[:extend_config].each do |path|
-        # RuboCop plugins often modify the default configuration in-place
-        # (e.g., load custom base configurations)
-        #
-        # So, we provide our blank configuration as a default one,
-        # to make sure all modifications have been applied
-        RuboCop::ConfigLoader.instance_variable_set(:@default_configuration, config)
-
-        path = Standard::FileFinder.new.call(path, Dir.pwd)
-
-        extension = RuboCop::ConfigLoader.load_file(path)
-
-        config = RuboCop::ConfigLoader.merge_with_default(extension, path)
+          user_rubocop_yaml_path = Standard::FileFinder.new.call(path, Dir.pwd)
+          extension = RuboCop::ConfigLoader.load_file(user_rubocop_yaml_path)
+          RuboCop::ConfigLoader.merge_with_default(extension, path)
+        end
       end
-
-      # Restore default configuration to the original state
-      RuboCop::ConfigLoader.instance_variable_set(:@default_configuration, orig_default_config)
-
-      # The Enabled key is an unused artefact of #merge_with_default
-      config["AllCops"].delete("Enabled")
-
-      config.to_h
     end
 
     def merge_standard_and_user_all_cops!(options_config, extended_config)
@@ -63,8 +47,21 @@ class Standard::CreatesConfigStore
       end
     end
 
-    def except(hash, keys)
-      hash.reject { |key, _| keys.include?(key) }.to_h
+    def fake_out_rubocop_default_configuration(options_config)
+      og_default_config = RuboCop::ConfigLoader.instance_variable_get(:@default_configuration)
+      result = yield blank_rubocop_config(options_config)
+      RuboCop::ConfigLoader.instance_variable_set(:@default_configuration, og_default_config)
+      result
+    end
+
+    # Blank configuration object to merge extensions into, with all known
+    # AllCops keys set to avoid warnings about unknown properties
+    def blank_rubocop_config(example_config)
+      RuboCop::Config.new(example_config.to_h.slice("AllCops"), "")
+    end
+
+    def except(hash_or_config, keys)
+      hash_or_config.to_h.reject { |key, _| keys.include?(key) }.to_h
     end
   end
 end
