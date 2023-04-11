1703e2ae9e5a05a0f5c8838193b2604009a9b7ed
Justin Searls <searls@gmail.com>
2023-04-11 10:45:58 -0400

    require this be set

--- lib/standard/plugin/combines_plugin_configs.rb
+++ lib/standard/plugin/combines_plugin_configs.rb
@@ -6,7 +6,7 @@ module Standard
         @merges_plugins_into_rubocop_config = MergesPluginsIntoRubocopConfig.new
       end
 
-      def call(options_config, standard_config, permit_merging: false)
+      def call(options_config, standard_config, permit_merging:)
         plugins = @initializes_plugins.call(standard_config[:plugins])
         @merges_plugins_into_rubocop_config.call(options_config, standard_config, plugins, permit_merging: permit_merging)
       end
