69f77220f314a318275467d06adea20b6493b430
Justin Searls <searls@gmail.com>
2023-04-12 21:59:53 -0400

    Attempt to fix this test to load a real configuration, since it just broke again

--- test/standard/runners/genignore_test.rb
+++ test/standard/runners/genignore_test.rb
@@ -9,30 +9,23 @@ class Standard::Runners::GenignoreTest < UnitTest
   end
 
   def test_todo_generated
-    FileUtils.rm_rf("tmp/genignore_test")
-    FileUtils.mkdir_p("tmp/genignore_test")
-    FileUtils.cp_r("test/fixture/genignore/.", "tmp/genignore_test")
+    FileUtils.rm_rf("dont_call_it_tmp/genignore_test")
+    FileUtils.mkdir_p("dont_call_it_tmp/genignore_test")
+    FileUtils.cp_r("test/fixture/genignore/.", "dont_call_it_tmp/genignore_test")
 
-    Dir.chdir("tmp/genignore_test") do
-      @subject.call(create_config("../../config/base.yml"))
+    Dir.chdir("dont_call_it_tmp/genignore_test") do
+      config = Standard::BuildsConfig.new.call(["--config", "../../config/base.yml"])
+      @subject.call(config)
     end
 
-    assert File.exist?("tmp/genignore_test/.standard_todo.yml")
+    assert File.exist?("dont_call_it_tmp/genignore_test/.standard_todo.yml")
 
     expected_yaml = {"ignore" => [
-      {"errors_one.rb" => ["Style/Documentation", "Style/FrozenStringLiteralComment", "Lint/AssignmentInCondition"]},
-      {"errors_two.rb" => ["Lint/UselessAssignment", "Style/FrozenStringLiteralComment", "Style/StringLiterals"]}
+      {"errors_one.rb" => ["Lint/AssignmentInCondition"]},
+      {"errors_two.rb" => ["Lint/UselessAssignment"]}
     ]}
-    assert_equal expected_yaml, YAML.load_file("tmp/genignore_test/.standard_todo.yml")
-  end
-
-  private
-
-  def create_config(config_path)
-    store = RuboCop::ConfigStore.new.tap do |config_store|
-      config_store.options_config = config_path
-    end
-
-    Standard::Config.new(nil, ["."], {}, store)
+    assert_equal expected_yaml, YAML.load_file("dont_call_it_tmp/genignore_test/.standard_todo.yml")
+  ensure
+    FileUtils.rm_rf("dont_call_it_tmp")
   end
 end
