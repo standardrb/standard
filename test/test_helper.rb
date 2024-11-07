e8a3240048d3e7e5c28c1beaa6a9c4d3b19c7655
Matt Jankowski <matt@jankowski.online>
2024-11-07 12:22:43 -0500

    Use `test_frameworks` simplecov profile

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 begin
   require "simplecov"
   SimpleCov.start do
-    add_filter "vendor"
+    load_profile "test_frameworks"
   end
 rescue LoadError
 end
